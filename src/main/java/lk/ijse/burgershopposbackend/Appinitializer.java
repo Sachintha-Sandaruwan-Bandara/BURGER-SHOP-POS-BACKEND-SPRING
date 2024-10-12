26542385bdd541122865c580b28aa7b43de74301
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-10-12 21:56:56 +0530

    classes added to layers

--- src/main/java/lk/ijse/burgershopposbackend/Appinitializer.java
+++ src/main/java/lk/ijse/burgershopposbackend/Appinitializer.java
@@ -14,5 +14,32 @@ package lk.ijse.burgershopposbackend;
  @created 10/11/2024 - 10:39 PM 
 */
 
-public class Appinitializer {
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.burgershopposbackend.config.WebAppConfig;
+import lk.ijse.burgershopposbackend.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Appinitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
+    }
+
 }
